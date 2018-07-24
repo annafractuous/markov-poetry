2fd5a9c2dcf21431e5f840722340ba24578f9440
annafractuous <anna.rasshivkina@flatironschool.com>
2018-07-23 23:17:03 -0400

    doing a reasonably good job formatting input text

--- scripts/app.js
+++ scripts/app.js
@@ -15,7 +15,38 @@ var App = {
     },
 
     createLibrary(text) {
-        console.log(text);
+        var words = this.formatText(text);
+        var grams = {};
+        var order = 2;
+
+        for (var i = 0, l = words.length; i < l - order; i++) {
+            var gram, nextWord;
+
+            if (words[i].match(/\.|\?|;|!/)) continue;
+            
+            gram = words.slice(i, i + order).join(' ');
+            gram = this.stripPunctuation(gram);
+
+            nextWord = words[i + order];
+            nextWord = this.stripPunctuation(nextWord);
+            
+            if (!grams[gram]) grams[gram] = [];
+
+            grams[gram].push(nextWord);
+        }
+
+        console.log(grams);
+    },
+
+    formatText(text) {
+        text = text.replace(/[^\sA-Za-z;.!?'"‘’“”]+/g, '');
+        text = text.replace(/\s{2,}/g, ' ');
+        
+        return text.split(' ');
+    },
+    
+    stripPunctuation(text) {
+        return text.replace(/[;.!?"‘’“”]*/g, '');
     }
 }
 
